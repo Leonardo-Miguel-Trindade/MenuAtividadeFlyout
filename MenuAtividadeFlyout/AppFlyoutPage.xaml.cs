a7a0c3513fe5260ea005906e047b90774ce64fd3
Leonardo Miguel Trindade <trindadeleo02@gmail.com>
2026-09-10 21:24:14 -0300

    Menu AppFlyoutPage.Xaml criado e formatado

---
+++ MenuAtividadeFlyout/AppFlyoutPage.xaml.cs
@@ -0,0 +1,29 @@
+namespace MenuAtividadeFlyout;
+
+public partial class AppFlyoutPage : FlyoutPage
+{
+	public AppFlyoutPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OndemonslayerClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnnarutoClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnonepieceClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnonepunchmanClicked(object sender, EventArgs e)
+    {
+
+    }
+}
